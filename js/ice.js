719480da57b3ea7b9857fde6e978169f15d3f7be
dabbler0 <dab1998@gmail.com>
2013-12-24 13:14:11 -0500

    Rudiments...

--- js/ice.js
+++ js/ice.js
@@ -1,175 +1,141 @@
 (function() {
-  var Block, BlockPaper, Socket;
+  var Block, Socket, State, defaultState, defaultStateElement;
 
-  Socket = (function() {
-    function Socket(type, child) {}
+  State = (function() {
+    /*
+    # RecomputeState is just a stack.
+    */
 
-    return Socket;
+    function State(elements) {
+      var element, _i, _len;
+      this.stack = {
+        next: null,
+        element: null
+      };
+      for (_i = 0, _len = elements.length; _i < _len; _i++) {
+        element = elements[_i];
+        this.push(element);
+      }
+    }
+
+    State.prototype.pop = function() {
+      return this.stack = this.stack.next;
+    };
+
+    State.prototype.push = function(element) {
+      var new_stack;
+      new_stack = {
+        next: this.stack,
+        element: element
+      };
+      return this.stack = new_stack;
+    };
+
+    State.prototype.first = function() {
+      return this.stack.element;
+    };
+
+    return State;
 
   })();
 
-  Block = (function() {
-    /*
-    # A Block is the tree node associated with a draggable ICE Block.
-    */
+  /*
+  # The "default" state for when we know nothing about a block or field
+  */
 
-    function Block(parent, type, sockets) {
-      this.parent = parent;
-      this.sockets = sockets || [];
-      this.type = type;
-      this.previous = null;
-      this.next = null;
-      this.block = new BlockPaper(this, {});
+
+  defaultStateElement = function() {
+    return {
+      multiline: false
+    };
+  };
+
+  defaultState = function() {
+    return new State([defaultStateElement()]);
+  };
+
+  /*
+  # A Block is the draggable wrapper for Sockets
+  */
+
+
+  Block = (function() {
+    function Block(prev, next) {
+      this.prev = prev;
+      this.next = next;
+      this.paper = null;
+      this.multiline = false;
     }
 
-    Block.prototype.toString = function() {
-      var socket;
-      return ((function() {
-        var _i, _len, _ref, _results;
-        _ref = this.sockets;
-        _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          socket = _ref[_i];
-          _results.push(socket.toString());
+    Block.prototype.moveTo = function(socket) {
+      this.prev.empty();
+      this.prev = null;
+      socket.empty();
+      this.prev = socket;
+      this.next = socket.next;
+      socket.insert(this);
+      return this.next.recompute(new State([
+        {
+          multiline: this.multiline
         }
-        return _results;
-      }).call(this)).join('');
+      ]));
+    };
+
+    Block.prototype.recompute = function(state) {
+      state.push(defaultStateElement());
+      return this.next.recompute(state);
+    };
+
+    Block.prototype._endRecompute = function(state, next) {
+      this.multiline = state.first.multiline;
+      state.pop();
+      if (next.type !== 'connection') {
+        state.first.multiline = this.multiline;
+      }
+      return next.recompute(state);
     };
 
     Block.prototype.stringify = function() {
-      if (this.next != null) {
-        return this.toString() + '\n' + this.next.stringify();
-      } else {
-        return this.toString();
-      }
+      return this.next.stringify;
     };
 
     return Block;
 
   })();
 
-  BlockPaper = (function() {
-    function BlockPaper(tree, options) {
-      options = _.extend({
-        position: new paper.Point(0, 0),
-        width: 100
-      }, options);
-      this._width = options.width;
-      this.tree = tree;
-      this.block = new paper.Path({
-        segments: [[0, 0], [10, 0], [20, 5], [30, 0], [this._width, 0], [this._width, 30], [30, 30], [20, 35], [10, 30], [0, 30], [0, 0]],
-        fillColor: '#000'
-      });
-      this.drop = new paper.Path({
-        segments: [[0, 25], [10, 25], [20, 30], [30, 25], [100, 25], [100, 35], [30, 35], [20, 40], [10, 35], [0, 35], [0, 25]]
-      });
-      this.hang = new paper.Path({
-        segments: [[0, 5], [10, 5], [20, 10], [30, 5], [100, 5], [100, -10], [30, -10], [20, -5], [10, -10], [0, -10], [0, 5]]
-      });
-      this.unit = new paper.Group([this.block, this.drop, this.hang]);
-      this.unit.translate(options.position);
+  Socket = (function() {
+    function Socket(prev, next) {
+      this.prev = prev;
+      this.next = next;
+      this.paper = null;
+      this.multiline = false;
+      this.type = null;
     }
 
-    BlockPaper.prototype.width = function(set) {
-      if (set != null) {
-        this.block.segments[4].point.x += set - this._width;
-        this.block.segments[5].point.x += set - this._width;
-        return this._width = set;
+    Socket.prototype.empty = function() {
+      return this.next = this.next.next;
+    };
+
+    Socket.prototype.insert = function(next) {
+      return this.next = next;
+    };
+
+    Socket.prototype.recompute = function(state) {
+      if (type === 'end') {
+        return this.block._endRecompute(state, this.next);
       } else {
-        return this._width;
+        return this.next.recompute(state);
       }
     };
 
-    return BlockPaper;
+    Socket.prototype.stringify = function() {
+      var string;
+      return string = typeof value === 'string' || value instanceof String ? value : '';
+    };
+
+    return Socket;
 
   })();
-
-  window.onload = function() {
-    var canvas, dragging, i, paths, tool, _i, _results;
-    canvas = document.getElementById('canvas');
-    paper.setup(canvas);
-    paths = [];
-    dragging = [];
-    _results = [];
-    for (i = _i = 1; _i <= 5; i = ++_i) {
-      paths.push(new BlockPaper(null, {
-        position: new paper.Point([i * 100, i * 100])
-      }));
-      tool = new paper.Tool();
-      tool.onMouseDown = function(event) {
-        var path, _j, _len, _results1;
-        dragging.length = 0;
-        _results1 = [];
-        for (_j = 0, _len = paths.length; _j < _len; _j++) {
-          path = paths[_j];
-          if (path.block.contains(event.point)) {
-            dragging.push(path);
-            path.width(100 + path.width());
-            break;
-          } else {
-            _results1.push(void 0);
-          }
-        }
-        return _results1;
-      };
-      tool.onMouseDrag = function(event) {
-        var other, path, x, _j, _k, _len, _len1, _results1;
-        _results1 = [];
-        for (x = _j = 0, _len = dragging.length; _j < _len; x = ++_j) {
-          path = dragging[x];
-          path.unit.translate(event.delta);
-          for (_k = 0, _len1 = paths.length; _k < _len1; _k++) {
-            other = paths[_k];
-            other.drop.fillColor = null;
-          }
-          _results1.push((function() {
-            var _l, _len2, _results2;
-            _results2 = [];
-            for (_l = 0, _len2 = paths.length; _l < _len2; _l++) {
-              other = paths[_l];
-              if (other !== path && path.hang.getIntersections(other.drop).length > 0) {
-                other.drop.fillColor = 'red';
-                break;
-              } else {
-                _results2.push(void 0);
-              }
-            }
-            return _results2;
-          })());
-        }
-        return _results1;
-      };
-      _results.push(tool.onMouseUp = function(event) {
-        var other, path, x, _j, _k, _len, _len1, _results1;
-        _results1 = [];
-        for (x = _j = 0, _len = dragging.length; _j < _len; x = ++_j) {
-          path = dragging[x];
-          for (_k = 0, _len1 = paths.length; _k < _len1; _k++) {
-            other = paths[_k];
-            other.drop.fillColor = null;
-          }
-          _results1.push((function() {
-            var _l, _len2, _results2;
-            _results2 = [];
-            for (_l = 0, _len2 = paths.length; _l < _len2; _l++) {
-              other = paths[_l];
-              if (other !== path && path.hang.getIntersections(other.drop).length > 0) {
-                other.drop.fillColor = 'red';
-                console.log(other.drop.position.toString(), path.hang.position.toString());
-                path.unit.translate(other.drop.position.subtract(path.hang.position));
-                break;
-              } else {
-                _results2.push(void 0);
-              }
-            }
-            return _results2;
-          })());
-        }
-        return _results1;
-      });
-    }
-    return _results;
-  };
 
 }).call(this);
 
--- src/ice.coffee
+++ src/ice.coffee
@@ -1,177 +1,101 @@
-class Socket
-  constructor: (type, child) ->
-    switch type
-      when 'static'
-      when 'inline'
-      when 'block'
+class State
+  ###
+  # RecomputeState is just a stack.
+  ###
+  constructor: (elements) ->
+    @stack =
+      next: null
+      element: null
+    for element in elements
+      @push element
 
-class Line
-  constructor: (parent, type, sockets) ->
+  pop: -> @stack = @stack.next
 
-    # Tree 
-    @parent = parent
-    @sockets = sockets or []
-    @type = type
+  push: (element) ->
+    new_stack =
+      next: @stack
+      element: element
+    @stack = new_stack
+  
+  first: -> @stack.element
 
-    # Next and previous blocks
-    @previous = null
-    @next = null
+###
+# The "default" state for when we know nothing about a block or field
+###
+defaultStateElement = ->
+  # TODO Metadata
+  multiline: false
 
-    # Block associated with this object
-    @block = new BlockPaper this, {}
+defaultState = -> new State [defaultStateElement()]
 
+###
+# A Block is the draggable wrapper for Sockets
+###
 class Block
-  ###
-  # A Block is the tree node associated with a draggable ICE Block.
-  ###
-  constructor: (parent, type, sockets) ->
-
-    # Tree 
-    @parent = parent
-    @sockets = sockets or []
-    @type = type
-
-    # Next and previous blocks
-    @previous = null
-    @next = null
-
-    # Block associated with this object
-    @block = new BlockPaper this, {}
-
-  toString: -> (socket.toString() for socket in @sockets).join ''
+  constructor: (@prev, @next) ->
+    @paper = null
+    
+    #TODO Metadata
+    @multiline = false
   
+  moveTo: (socket) ->
+    # Unlink ourselves from the list
+    @prev.empty()
+    @prev = null
+    
+    # Remove whatever value is in there, if it exists
+    socket.empty()
+    
+    # Insert ourselves into the socket
+    @prev = socket
+    @next = socket.next
+    socket.insert this
+
+    # Trigger a recompute
+    @next.recompute new State [
+      # TODO Metadata
+      multiline: @multiline
+    ]
+
+  recompute: (state) ->
+    # Block recomputes push to the stack
+    state.push defaultStateElement()
+    @next.recompute state
+
+  _endRecompute: (state, next) ->
+    # TODO Metadata
+    @multiline = state.first.multiline
+    
+    # TODO Metadata
+    state.pop() # Pop from the stack
+    if next.type isnt 'connection' then state.first.multiline = @multiline # All parents are multiline
+    
+    # Pass on to the next recompute
+    next.recompute state
+
   stringify: ->
-    if @next? then @toString() + '\n' + @next.stringify()
-    else @toString()
-
-class SocketPaper
-  constructor: (tree, options) ->
-    options = _.extend {
-      position: new paper.Point 0, 0
-      width: 20
-    }
-
-class BlockPaper
-  constructor: (tree, options) ->
-    options = _.extend {
-      position: new paper.Point 0, 0
-      width: 100
-    }, options
-
-    @_width = options.width
-    @tree = tree
-    @block = new paper.Path
-      segments: [
-        [0, 0]
-
-        # Tab
-        [10, 0]
-        [20, 5]
-        [30, 0]
-
-        # Right edge
-        [@_width, 0]
-        [@_width, 30]
-
-        # Tab
-        [30, 30]
-        [20, 35]
-        [10, 30]
-
-        # Left edge
-        [0, 30]
-        [0, 0]
-      ]
-      fillColor: '#000'
-
-    @drop = new paper.Path
-      segments: [
-        # Bottom bit
-        [0, 25]
-        [10, 25]
-        [20, 30]
-        [30, 25]
-        [100, 25]
-
-        # Top bit
-        [100, 35]
-        [30, 35]
-        [20, 40]
-        [10, 35]
-        [0, 35]
-        [0, 25]
-      ]
-
-    @hang = new paper.Path
-      segments: [
-        # Bottom bit
-        [0, 5]
-        [10, 5]
-        [20, 10]
-        [30, 5]
-        [100, 5]
-
-        # Top bit
-        [100, -10]
-        [30, -10]
-        [20, -5]
-        [10, -10]
-        [0, -10]
-        [0, 5]
-      ]
-    
-    @unit = new paper.Group [@block, @drop, @hang]
-    @unit.translate options.position
-
-  width: (set) ->
-    if set?
-      @block.segments[4].point.x += set - @_width
-      @block.segments[5].point.x += set - @_width
-      @_width = set
-    else
-      return @_width
-
-  _recomputeWidth: ->
-    for socket in @tree.sockets
-
-window.onload = ->
-  canvas = document.getElementById 'canvas'
-  paper.setup canvas
-
-  paths = []
-  dragging = []
+    return @next.stringify
   
-  for i in [1..5]
-    paths.push new BlockPaper null,
-      position: new paper.Point [i * 100, i * 100]
-
-    tool = new paper.Tool()
+class Socket
+  constructor: (@prev, @next) ->
+    @paper = null
     
-    tool.onMouseDown = (event) ->
-      dragging.length = 0
-      for path in paths
-        if path.block.contains event.point
-          dragging.push path
-          path.width 100 + path.width()
-          break
+    # TODO Metadata
+    @multiline = false # Does this socket contain a multiline instance?
+    @type = null # in [connection, inline, indent, break, end]
+  
+  empty: -> @next = @next.next
 
-    tool.onMouseDrag = (event) ->
-      for path, x in dragging
-        path.unit.translate event.delta
-        for other in paths
-          other.drop.fillColor = null
-        for other in paths
-          if other isnt path and path.hang.getIntersections(other.drop).length > 0
-            other.drop.fillColor = 'red'
-            break
+  insert: (next) -> @next = next
+  
+  recompute: (state) ->
+    if type is 'end'
+      # If we are a block end socket, pass to the block's _endRecompute
+      @block._endRecompute state, @next
+    else
+      # TODO Metadata
+      @next.recompute state
 
-    tool.onMouseUp = (event) ->
-      for path, x in dragging
-        for other in paths
-          other.drop.fillColor = null
-        for other in paths
-          if other isnt path and path.hang.getIntersections(other.drop).length > 0
-            other.drop.fillColor = 'red'
-            console.log other.drop.position.toString(), path.hang.position.toString()
-            path.unit.translate other.drop.position.subtract path.hang.position
-            break
+  stringify: ->
+    string = if typeof value is 'string' or value instanceof String then value else ''
+    return string + @next.stringify()
