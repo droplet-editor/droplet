588d494ba18011e381545b365f4634eea19bf4e0
David Anthony Bau <dab1998@gmail.com>
2016-07-25 14:59:01 -0400

    Whoops, add necessary worker.js

---
+++ src/worker.js
@@ -0,0 +1,36 @@
+var modes = require('./modes.coffee');
+
+runningModes = {};
+
+console.log('Worker starting up.');
+
+onmessage = function(e) {
+  var data = e.data;
+
+  if (data.command === 'initMode') {
+    runningModes[data.id] = new modes[data.mode](data.modeOptions);
+  }
+  else if (data.command === 'preparse') {
+    if (data.id in runningModes && runningModes[data.id].preparse != null) {
+      try {
+        var result = runningModes[data.id].preparse(data.text, data.context);
+        postMessage({
+          id: data.id,
+          result: result
+        });
+      }
+      catch (e) {
+        console.log('Parse error', e);
+        postMessage({
+          id: data.id,
+          result: null
+        });
+      }
+    } else {
+      postMessage({
+        id: data.id,
+        result: null
+      });
+    }
+  }
+};
