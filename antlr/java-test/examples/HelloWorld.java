8fa70ff2af61ab1d2b14fb01407697aa663cb059
Jeremiah Blanchard <jeremiahblanchard@gmail.com>
2018-08-12 18:09:05 -0400

    Minor additions for debugging

--- antlr/java-test/examples/HelloWorld.java
+++ antlr/java-test/examples/HelloWorld.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-import bob.*;
-import static bob.*;
-
 public class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Hello world!"); //Comment
-        int jimmyDean = 10;
-        float sausage;
     }
-    public int bob = 10;
 }
